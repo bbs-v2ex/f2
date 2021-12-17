a57430bd4291d36e950cb5580f08455423c5817c
灰太狼 <57159300@qq.com>
2021-12-17 20:11:45 +0800

    添加吧int 转为 时分秒的函数

--- 2021_12_16_InSliceString.go
+++ 2021_12_16_InSliceString.go
@@ -1,5 +1,7 @@
 package f2
 
+import "math"
+
 func InSliceString(array_val string, array []string) bool {
 	for _, v := range array {
 		if v == array_val {
@@ -7,4 +9,22 @@ func InSliceString(array_val string, array []string) bool {
 		}
 	}
 	return false
+}
+
+func SliceChunkString(s []string, size int) [][]string {
+	if size < 1 {
+		return [][]string{}
+	}
+	length := len(s)
+	chunks := int(math.Ceil(float64(length) / float64(size)))
+	var n [][]string
+	for i, end := 0, 0; chunks > 0; chunks-- {
+		end = (i + 1) * size
+		if end > length {
+			end = length
+		}
+		n = append(n, s[i*size:end])
+		i++
+	}
+	return n
 }
